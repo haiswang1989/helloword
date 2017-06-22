2c2c3258b74c05681bbb9390f0789b8742ba4a5f
hansen.wang <hansen.wang@vipshop.com>
2017-06-22 18:17:10 +0800

    add code

---
+++ src/main/java/com/hiworld/thread/ThreadPool.java
@@ -0,0 +1,25 @@
+package com.hiworld.thread;
+
+import java.util.concurrent.Executors;
+
+public class ThreadPool {
+    
+    public static void main(String[] args) {
+        
+        //SynchronousQueue
+        Executors.newCachedThreadPool();
+        
+        //LinkedBlockingQueue
+        Executors.newFixedThreadPool(5);
+        
+        //ScheduledThreadPoolExecutor with DelayedWorkQueue
+        Executors.newScheduledThreadPool(5);
+        
+        //LinkedBlockingQueue
+        Executors.newSingleThreadExecutor();
+        
+        //ScheduledThreadPoolExecutor with DelayedWorkQueue
+        Executors.newSingleThreadScheduledExecutor();
+    }
+    
+}
