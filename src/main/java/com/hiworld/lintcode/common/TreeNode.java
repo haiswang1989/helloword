f662c85542414f8615de8e2808d5293f62a5223b
hansen.wang <hansen.wang@vipshop.com>
2017-07-06 09:55:56 +0800

    upgrade version

---
+++ src/main/java/com/hiworld/lintcode/common/TreeNode.java
@@ -0,0 +1,16 @@
+package com.hiworld.lintcode.common;
+
+/**
+ * 
+ * <p>Description:</p>
+ * @author hansen.wang
+ * @date 2017年7月5日 下午1:50:54
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
