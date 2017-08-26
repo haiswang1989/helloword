d8ef79559cc94726dd1c2c55870d97d890b1daf1
unknown <790010204@qq.com>
2017-08-26 14:23:04 +0800

    add code

--- src/main/java/com/hiworld/lintcode/medium/ArrayContinuousSubarraySum.java
+++ src/main/java/com/hiworld/lintcode/medium/ArrayContinuousSubarraySum.java
@@ -17,7 +17,7 @@ public class ArrayContinuousSubarraySum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] A = {-3, 1, 3, -3, 4};
+		int[] A = {-3,-2,-1};
 		ArrayContinuousSubarraySum arrayContinuousSubarraySum = new ArrayContinuousSubarraySum();
 		ArrayList<Integer> rets = arrayContinuousSubarraySum.continuousSubarraySum(A);
 		System.out.println(Arrays.toString(rets.toArray()));
@@ -29,44 +29,57 @@ public class ArrayContinuousSubarraySum {
      */
     public ArrayList<Integer> continuousSubarraySum(int[] A) {
         // Write your code here
-    	int maxFromIndex = 0;
-    	int maxEndIndex = 0;
-    	int max =  A[0];
+    	//记录历史最大的和,以及beginIndex和endIndex
+    	int historyMaxFromIndex = -1;
+    	int historyMaxEndIndex = -1;
+    	int historyMax =  Integer.MIN_VALUE;
     	
-    	int fromIndex = 0;
-    	int endIndex = 0;
-    	int currMax = A[0];
-    	int sum = A[0];
+    	//记录该次循环的最大值和对应的index
+    	int fromIndex = -1;
+    	int endIndex = -1;
+    	int currMax = Integer.MIN_VALUE;
+    	int sum = 0;
     	
     	int length = A.length;
-    	
-    	for(int i=1; i<length; i++) {
-    		if(sum > 0) {
+    	for(int i=0; i<length; i++) {
+    		sum += A[i];
+    		if(sum >= 0) { //如果和大于0,那么就可以继续往下累加
     			if(sum > currMax) {
+    				if(fromIndex == -1) {
+    					fromIndex = i;
+    				}
+    				
     				endIndex = i;
     				currMax = sum;
     			} 
+    		} else { //出现sum小于0,继续往下加就不符合条件了
+    			if(sum > historyMax && fromIndex == -1) { //这部分是对全负数的处理,如果直接是负数,就记录最小的负数
+    				historyMaxFromIndex = i;
+    				historyMaxEndIndex = i;
+    				historyMax = sum;
+    			} else if(currMax > historyMax) { //如果这次循环的最大值大于历史的最大值,那么就更新历史最大值的index和max值
+    				historyMaxFromIndex = fromIndex;
+    				historyMaxEndIndex = endIndex;
+    				historyMax = currMax;
+    			}
     			
-    			sum += A[i];
-    		} else { //出现sum小于0,那么这边需要更新开始索引
-    			maxFromIndex = fromIndex;
-    			maxEndIndex = endIndex;
-    			
-    			fromIndex = i;
-    			endIndex = i;
-    			
-    			sum = A[i]; 
+    			//清除当前循环的状态
+    			fromIndex = -1;
+    			endIndex = -1;
+    			sum = 0; 
+    			currMax = Integer.MIN_VALUE;
     		}
     	}
     	
-    	if(currMax > max) {
-    		maxFromIndex = fromIndex;
-			maxEndIndex = endIndex;
+    	if(currMax > historyMax) { //退出来以后,要处理一下最后的情况
+			historyMaxFromIndex = fromIndex;
+			historyMaxEndIndex = endIndex;
+			historyMax = currMax;
     	}
     	
     	ArrayList<Integer> ret = new ArrayList<>();
-    	ret.add(maxFromIndex);
-    	ret.add(maxEndIndex);
+    	ret.add(historyMaxFromIndex);
+    	ret.add(historyMaxEndIndex);
     	return ret;
     }
 
